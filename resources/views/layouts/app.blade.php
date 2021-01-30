dd9448531f7cb545fae339a4d08ccc9f485185de
Awan <m.awan.saifurrehman@gmail.com>
2021-01-30 17:43:41 +0500

    change title club to UIFA Champions

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -460,7 +460,7 @@
                                 <li class="pcoded-hasmenu">
                                     <a href="javascript:void(0)">
                                         <span class="pcoded-micon"><i class="ti-layout-grid2-alt"></i></span>
-                                        <span class="pcoded-mtext" data-i18n="nav.basic-components.main">Club</span>
+                                        <span class="pcoded-mtext" data-i18n="nav.basic-components.main">UIFA Champions</span>
                                         <span class="pcoded-mcaret"></span>
                                     </a>
                                     <ul class="pcoded-submenu">
@@ -468,7 +468,7 @@
                                             <a href="/main-club">
                                                 <span class="pcoded-micon"><i class="ti-angle-right"></i></span>
                                                 <span class="pcoded-mtext"
-                                                    data-i18n="nav.basic-components.alert">Main Club</span>
+                                                    data-i18n="nav.basic-components.alert">Main UIFA Champions</span>
                                                 <span class="pcoded-mcaret"></span>
                                             </a>
                                         </li>
